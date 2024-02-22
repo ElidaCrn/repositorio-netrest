e11c417533594d5a6afc5d9a92ebbdea8effa92e
Elida Barbosa Carneiro <elidacrn@hotmail.com>
2024-02-21 22:46:56 -0300

    Adicionado arquivos de API

---
+++ WebApplicationConversor/Controllers/ConversorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Drawing;
+
+namespace WebApplicationConversor.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorController : Controller
+    {
+      
+        [HttpGet("{valor}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<float> ConverterValor(String valor)
+        {
+            ResultadoCostumizado resultadoCostumizado = null;
+
+            decimal valorfloat = 0;
+          
+            if (!decimal.TryParse(valor, out valorfloat))
+
+            {
+                resultadoCostumizado = new ResultadoCostumizado(System.Net.HttpStatusCode.BadRequest, DateTime.Now, 0, "Valor Invalido");
+                return BadRequest(new JsonResult(resultadoCostumizado) { StatusCode = (int)resultadoCostumizado.StatusCode });
+            }
+            else
+            {
+                valorfloat = decimal.Parse(valor);
+            }
+
+            if (valorfloat < 1)
+            {
+                resultadoCostumizado = new ResultadoCostumizado(System.Net.HttpStatusCode.BadRequest, DateTime.Now, 0, "Valor Invalido");
+
+                return BadRequest(new JsonResult(resultadoCostumizado) { StatusCode = (int)resultadoCostumizado.StatusCode });
+            }
+            else
+            {
+                valorfloat = valorfloat * 9500000000000;
+                resultadoCostumizado = new ResultadoCostumizado(System.Net.HttpStatusCode.OK, DateTime.Now, valorfloat,"Processado com sucesso");
+
+
+                return Ok(new JsonResult(resultadoCostumizado) { StatusCode = (int)resultadoCostumizado.StatusCode });
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ WebApplicationConversor/Controllers/ResultadoCostumizado.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace WebApplicationConversor.Controllers
+{
+    public class ResultadoCostumizado
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+        public DateTime data { get; set; }
+
+        public decimal valor { get; set; }
+
+        public String error { get; private set; }
+
+
+        public ResultadoCostumizado(HttpStatusCode StatusCode, DateTime data, decimal valor, String error)
+        {
+            this.StatusCode = StatusCode;
+            this.data = data;
+            this.valor = valor;
+            this.error = error;
+        }
+    }
+
+    }
